ef863a2de0a37f1606f7a5fef1599743b069e038
fighter-666 <115720897+fighter-666@users.noreply.github.com>
2022-10-23 22:56:49 +0800

    Add files via upload

---
+++ jiaohuan.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+
+}
+
+//int main()
+//{
+//	/*short s = 0;
+//	int a = 10;
+//	printf("%d\n", sizeof(s = a + 5));
+//	printf("%d\n", s);
+//	return 0;*/
+//
+//	int a = 0;
+//	char b = 'w';
+//	int arr[10] = { 0 };
+//	printf("%d\n", sizeof(a));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(a));
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", sizeof(int [10]));
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("a = %d b =%d\n", a, b);
+//	return 0;
+//}
